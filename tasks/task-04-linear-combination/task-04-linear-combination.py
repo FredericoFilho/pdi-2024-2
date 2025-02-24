bedf62ea2be14edcfe2df62c291c3cf0fcc64127
FredericoFilho <fgogf@ic.ufal.br>
2025-02-24 17:28:02 -0300

    Task 04 completed

--- tasks/task-04-linear-combination/task-04-linear-combination.py
+++ tasks/task-04-linear-combination/task-04-linear-combination.py
@@ -20,7 +20,13 @@ def linear_combination(i1: np.ndarray, i2: np.ndarray, a1: float, a2: float) -> 
         raise ValueError("Input images must have the same dimensions.")
 
     ### START CODE HERE ###
-    ### TODO
+    
+    i_out = cv2.addWeighted(i1, a1, i2, a2, 0)
+
+    cv2.imshow(i_out)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
     ### END CODE HERE ###
 
     return None
